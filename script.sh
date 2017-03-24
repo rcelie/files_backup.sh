7b52468c4df88448db7535836027b4ec56291a1b
Robin CELIE GRIMAL <robin.celie@gmail.com>
2017-03-25 10:14:36 +1100

    Update script.sh
    Corrected typos

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@
 # file3.jsp
 # directory
 
-# Checks thaht the correct user is executing the script; returns error and stops if not
+# Checks that the correct user is executing the script; returns error and stops if not
 if [ $(whoami) != 'user' ]; then
         echo "Must be user to run $0"
         exit 1;
@@ -18,10 +18,10 @@ fi
 # Contains all files the script will copy to backup directory
 files_array=(file.jsp file2.jsp file3.jsp directory)
 
-# Gets today's date for backup directory name
+# Gets the date for backup directory name
 date_value=$(date '+%Y-%m-%d')
 
-# Make a directory using today's date
+# Make a directory using the date
 mkdir /path/to/backup/directory/"$date_value"
 
 # Loops over the whole array and copy files/directories recursively to given directory with current rights
