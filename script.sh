336999819fd8d2e938447c7369f7883a011f438d
Robin CELIE GRIMAL <robin.celie@gmail.com>
2017-04-03 18:53:31 +1100

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,7 +11,7 @@
 # directory
 
 # Checks that the correct user is executing the script; returns error and stops if not
-if [ $(whoami) != 'user' ]; then
+if [ "$(whoami)" != 'user' ]; then
         echo "Must be user to run $0"
         exit 1;
 fi
@@ -29,45 +29,46 @@ path='/home/'$USER'backup'
 current_path=$(pwd)
 
 #tests for existing backup directory.
-if [ -d "$path/$date_value.tar.gz" ] ; then
+if [ -f "$path/$date_value.tar.gz" ] ; then
 # If backup direcotry exist, do
 	echo "A $date_value.tar;gz directory already exist."
-	
+
+askUpdate() {	
 	read -p "Do you wish to update directory ? (Y/N)" -n 1 -r
+}
+	user_answer="$(askUpdate)"
 	
 	echo # Moves to new line; for user experience purpose only
 	
-	case "$REPLY" in
+	case $user_answer in
 # Case when user answered "Y" or "y" (Yes), do
-	" [[ $REPLY =~ ^[Yy]$ ]] " )
+	^[Yy]$ ) tar xf "$path/$date_value.tar.gz" 
 	
-		# Extract archive
-		tar xf "$path/$date_value.tar.gz"
+	rsync --update -raz --progress --include="$current_path${files_array[*]}" "$path/$date_value" --exclude="*"
 	
-		# Synchronise directories
-		rsync --update -raz --progress --include="$current_path${files_array[@]}" "$path/$date_value" --exclude="*"
+	exit 0;
 	;;
+	
 # Case when user andwered anything else, do
-	" [[ $REPLY =~ ^[*]$ ]] " )
-		echo "Script didn't make any change and stopped itself."
-		exist 1;
+	^[*]$ ) echo "Didn't update directory."
+	
+		exit 1;
 	esac
 	
-# Else if backup direcotry doesn't existr yet, do
+# Else if backup directory doesn't exist yet
 else
 # Make a directory using the date
 mkdir "$path/$date_value"
 
 # Loops over the whole array and copy files/directories recursively to given directory with current rights
-for i in ${files_array[@]}; do
-		cp -ar ${i} /home/robin/backup/by_date/"$date_value"/
+for i in "${files_array[@]}"; do
+		cp -ar "${i}" /home/robin/backup/by_date/"$date_value"/
 done
 
 # Goes to backup directory
-cd "$path"
+cd "$path" || return
 
 # Compress backup directory into tarball AND(=&&) removes it if successfull
 tar cfM "$date_value.tar.gz" "$date_value" && rm -Rf "$date_value"
 
 fi
-exit 0;
